dcb97b02a056ca31cd90ffd58f9006ae655de656
Quinnpapasote89 <admaor2000@gmail.com>
2022-11-17 17:56:36 -0600

    Implementado diseños

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {
     path:'',
     component: InicioComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
   }
 ];
 
