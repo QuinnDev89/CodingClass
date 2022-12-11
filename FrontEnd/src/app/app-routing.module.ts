a1520a8be456c6278b445d4bbb31045619e40a5b
Quinnpapasote89 <admaor2000@gmail.com>
2022-12-10 21:53:24 -0600

    Update rutas

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,22 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ArbolesApuntesComponent } from './arboles-apuntes/arboles-apuntes.component';
+import { BlogComponent } from './blog/blog.component';
 import { ColasApuntesComponent } from './colas-apuntes/colas-apuntes.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
 import { GrafosApuntesComponent } from './grafos-apuntes/grafos-apuntes.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ListasApuntesComponent } from './listas-apuntes/listas-apuntes.component';
+import { LogingComponent } from './loging/loging.component';
 import { PilasApuntesComponent } from './pilas-apuntes/pilas-apuntes.component';
+import { SingnupComponent } from './singnup/singnup.component';
 import { TeachersComponent } from './teachers/teachers.component';
 
 const routes: Routes = [
-  {path:'home',component: InicioComponent},
+  {path:'',component: InicioComponent},
   {path:'about',component: AboutComponent},
   {path: 'arboles', component: ArbolesApuntesComponent},
   {path: 'colas', component: ColasApuntesComponent},
   {path: 'grafos', component: GrafosApuntesComponent},
   {path: 'listas', component: ListasApuntesComponent},
   {path: 'pilas', component: PilasApuntesComponent},
-  {path: 'teacher', component: TeachersComponent}
+  {path: 'teacher', component: TeachersComponent},
+  {path: 'courses', component: CoursesComponent},
+  {path: 'sign', component:SingnupComponent},
+  {path: 'login', component:LogingComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'blog', component:BlogComponent}
 ];
 
 @NgModule({
